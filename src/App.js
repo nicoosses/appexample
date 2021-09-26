9b78e95435ba6860cc903bbf977ea35811bf59f7
nicoosses <jnicoosses@gmail.com>
2021-09-26 18:03:35 -0300

    Desafio 2 NavBar Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import NavBar from "./Components/Navbar/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() { 
+    return (
+      <div className="App">
+       
+           <NavBar/>
+           <h1 className="Titulo">Front-E Commerce</h1>
+
+        
+      </div>
+
   );
+}
 }
 
 export default App;
---
+++ src/Components/Navbar/NavBar.js
@@ -0,0 +1,61 @@
+import '../Navbar/NavBar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+
+
+
+
+const NavBar = () => {
+
+    return (
+        
+      <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+        <div className="container-fluid">
+        <a class="navbar-brand" href="#"><FontAwesomeIcon icon= {faHome}> </FontAwesomeIcon></a>
+       
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+        
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Who</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">What</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Templates</a></li>
+            <li><a class="dropdown-item" href="#">Customization</a></li>
+            <li><a class="dropdown-item" href="#">Social Medias</a></li>
+          </ul>
+        </li>
+       
+
+      </ul>
+          
+          </div>
+          
+       
+    </div>
+</nav>
+
+      );
+
+
+};
+
+export default NavBar;
