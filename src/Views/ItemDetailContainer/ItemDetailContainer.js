bce908a37138b4af5e66ab694fbceb7ac2560d7f
nicoosses <jnicoosses@gmail.com>
2021-10-19 14:50:48 -0300

    ItemDetail !

--- src/Views/ItemDetailContainer/ItemDetailContainer.js
+++ src/Views/ItemDetailContainer/ItemDetailContainer.js
@@ -10,32 +10,37 @@ const UserDetail = ({ match }) => {
 	const [usuario, setUser] = useState([]);
 
 	useEffect(() => {
-		axios(`https://api.github.com/users?id=${userID}`).then((res) =>
+		axios(`https://api.github.com/users/${userID}`).then((res) =>
 			setUser(res.data)
 		);
 	}, [userID]);
 
 
 	return (
+		<div>
+
 		<div className='ItemDetail' style={{ padding: 40 }}>
 			<h1>User Detail</h1>
-			{usuario.map((user) => {
-				return (
-					<Card key={user.id}>
-						<Image src={user.avatar_url} wrapped ui={false} />
+			console.log(usuario);
+				{/* <h2>User Id: {usuario.login}</h2>
+				<h2>User name: {usuario.login}</h2> */}
+				
+					 <Card key={usuario.id}>
+						<Image src={usuario.avatar_url} wrapped ui={false} />
 						<Card.Content>
-							<Card.Header>{user.login}</Card.Header>
+							<Card.Header>{usuario.login}</Card.Header>
 							<Card.Meta>
-								<span>{user.type}</span>
+								<span>{usuario.type}</span>
 							</Card.Meta>
-							<Card.Description>{user.url}</Card.Description>
+							<Card.Description>{usuario.url}</Card.Description>
 						</Card.Content>
-					</Card>
-					
-				);		
-				})}
+					</Card> 
+		</div>
+
 		</div>
 		
+
+
 	);
 	
 };
